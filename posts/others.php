0194a4cae582954f4e9a65557f192131875f318a
BorisChibanga <borisbwalyamalama@gmail.com>
2023-11-30 10:13:54 -0800

    same

--- posts/others.php
+++ posts/others.php
@@ -10,6 +10,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -19,6 +23,7 @@
 <body>
 
     <div class="container mt-5">
+        <div class="card-body">
 
         <!-- Display any info -->
         <?php if(isset($_REQUEST['info'])){ ?>
@@ -50,7 +55,7 @@
                 </div>
             <?php }?>
         </div>
-       
+        </div>
     </div>
 
     <!-- Bootstrap JS -->
