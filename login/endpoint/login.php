5c18038dd2a9b33fbf522fa0f1ece054048e791a
BorisChibanga <borisbwalyamalama@gmail.com>
2023-12-03 13:46:33 -0800

    login is now working perfectly

---
+++ login/endpoint/login.php
@@ -0,0 +1,41 @@
+<?php
+include ('../conn/conn.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT `password` FROM `tbl_user` WHERE `username` = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        $stored_password = $row['password'];
+
+        if ($password === $stored_password) {
+            echo "
+            <script>
+                alert('Login Successfully!');
+                window.location.href = 'http://localhost/community_management/Admin/dashboard/dashboard.html';
+            </script>
+            "; 
+        } else {
+            echo "
+            <script>
+                alert('Login Failed, Incorrect Password!');
+                window.location.href = 'login/login.php';
+            </script>
+            ";
+        }
+    } else {
+        echo "
+            <script>
+                alert('Login Failed, User Not Found!');
+                window.location.href = 'login/login.php';
+            </script>
+            ";
+    }
+}
+
+?>
