027f4df9cf01e0ac6e22682effa90820462281d3
=jevis <jevisbwalya.m@gmail.com>
2023-12-24 12:10:20 +0200

    final changes

--- community_management.sql
+++ community_management.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 06:35 AM
+-- Generation Time: Dec 24, 2023 at 11:09 AM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,14 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admins`
+-- Table structure for table `blog_data`
 --
 
-CREATE TABLE `admins` (
+CREATE TABLE `blog_data` (
   `id` int(11) NOT NULL,
-  `users` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL
+  `title` varchar(25) NOT NULL,
+  `content` varchar(50) NOT NULL,
+  `images` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blog_data`
+--
+
+INSERT INTO `blog_data` (`id`, `title`, `content`, `images`) VALUES
+(1, 'testing', 'this the ward project', ''),
+(3, 'hey', 'alnfjanksm;s', ''),
+(4, 'HEYKBJA', 'A;FBJANKJ;LM;LCXNBVJFBJBCLKMSL;', 'Snapchat-28365589.jpg'),
+(5, 'Heloo boris', 'This is good ', ''),
+(6, 'PROJECT BID INVITATION ', 'We want someone to run a project. To build a hospi', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`id`, `name`, `email`, `message`, `created_at`) VALUES
+(2, 'Kusanth Thankaian,', 'musonda@gmail.com', 'I love coding', '2023-12-18 19:12:34'),
+(3, 'Musonda Machila', 'musonda@gmail.com', 'Hello Namwanga Ward. I want to know for how long will the bridge project finish and you intend to do with the unfinished class block...?', '2023-12-24 01:14:24'),
+(4, 'Tomy Siapenga', 'tomsiapenga@gmail.com', 'What is going on?', '2023-12-24 09:56:14');
 
 -- --------------------------------------------------------
 
@@ -47,56 +82,46 @@ CREATE TABLE `messages` (
   `sent_from` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`id`, `message_title`, `message_purpose`, `message_descritption`, `sent_from`) VALUES
-(1, 'hey', 'testing the website', 'I am trying to see if the web is able to get the messages from the database', 'new user Boris Bwalya');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `posts`
+-- Table structure for table `tbl_user`
 --
 
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `post_title` varchar(20) NOT NULL,
-  `post_description` varchar(250) NOT NULL,
-  `sent_by` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `posts` varchar(255) NOT NULL,
-  `users` varchar(50) NOT NULL,
-  `role` varchar(20) NOT NULL,
-  `phone_number` varchar(20) NOT NULL,
-  `password` varchar(30) NOT NULL
+CREATE TABLE `tbl_user` (
+  `tbl_user_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `users` (`id`, `posts`, `users`, `role`, `phone_number`, `password`) VALUES
-(1, 'hey we have new projects', 'Boris', 'admin', '0778345937', '1234');
+INSERT INTO `tbl_user` (`tbl_user_id`, `first_name`, `last_name`, `contact_number`, `email`, `username`, `password`) VALUES
+(3, 'Boris', 'Bwalya', '977535840', 'prekanyama@gmail.com', 'admin', '12345'),
+(4, 'Jevis', 'Bwalya', '978071361', 'jevisbwaya.m@gmail.com', 'jevis', '1234'),
+(5, 'Mutale', 'Siamunyanga', '97824521', 'mutale@gmial.com', 'mutale', '1234'),
+(6, 'Tomy', 'Siapenga', '976985288', 'tomsiapenga@gmail.com', 'Tomy', '1234');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admins`
+-- Indexes for table `blog_data`
 --
-ALTER TABLE `admins`
+ALTER TABLE `blog_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -106,26 +131,26 @@ ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `posts`
+-- Indexes for table `tbl_user`
 --
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`tbl_user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admins`
+-- AUTO_INCREMENT for table `blog_data`
 --
-ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `blog_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `messages`
@@ -134,16 +159,10 @@ ALTER TABLE `messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `posts`
+-- AUTO_INCREMENT for table `tbl_user`
 --
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `tbl_user`
+  MODIFY `tbl_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
